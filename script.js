d9ab5bb52edfa20eb4b5adc4a62656fee19c49b4
Shekkinah2 <146448401+Shekkinah2@users.noreply.github.com>
2024-11-03 21:56:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn=document.getElementById("menu-btn");
+const navLinks=document.getElementById("nav-links")
+const menuBtnIcon=menuBtn.querySelector("i");
+menuBtn.addEventListener("click",(e)=>{
+    navLinks.classList.toggle("open");
+    const isopen=navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class",isopen?"ri-close-line":"ri-menu-line");
+});
+navLinks.addEventListener("click",(e)=>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class","ri-menu-line");
+});
