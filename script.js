cd65829f483948dabb11b67424c8b8f8401bd57b
Shekkinah <shekkinahbabu@gmail.com>
2024-11-10 23:39:23 +0530

    Second commit

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ navLinks.addEventListener("click",(e)=>{
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class","ri-menu-line");
 });
+const scrollRevealOption={
+    distance:"50px",origin:"bottom",duration:1000,
+};
+//header container
+scrollReveal().reveal(".header_container.section_subheader",{...scrollRevealOption,});
+scrollReveal().reveal(".header_container.h1",{...scrollRevealOption,delay:500});
+scrollReveal().reveal(".header_container.btn",{...scrollRevealOption ,delay:1000});
+// room container
+ScrollReveal().reveal(".room_card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  // feature container
+  ScrollReveal().reveal(".feature_card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  // gallery container
+  ScrollReveal().reveal(".gallery_card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
